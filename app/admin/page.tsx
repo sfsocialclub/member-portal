a8ed1961d9c222cacdf7847786e3bab2db43b706
Brian Lee <brianlee1597@gmail.com>
2024-12-14 20:28:47 -0800

    init

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,9 @@
-import { LogoutButton } from "../components/auth/LogoutButton";
+"use client";
 
 export default function AdminPage() {
-  return <>
-    <h1 className="block">Admin page</h1>
-  </>;
+  return (
+    <>
+      <h1 className="block">Admin page</h1>
+    </>
+  );
 }
