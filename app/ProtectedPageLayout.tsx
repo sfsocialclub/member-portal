5bcb93952f44ba1a771b5f271add0b938c2ede80
Brian Lee <brianlee1597@gmail.com>
2025-01-05 17:09:16 -0800

    init

--- app/ProtectedPageLayout.tsx
+++ app/ProtectedPageLayout.tsx
@@ -1,12 +1,18 @@
-import { Navbar } from "./components/NavLinks"
-import { useRouterWithOptimisticPathname } from "./hooks/useOptimisticRouter"
+import { Navbar } from "./components/NavLinks";
+import { useRouterWithOptimisticPathname } from "./hooks/useOptimisticRouter";
 
-export const ProtectedPageLayout = ({ children }: { children: React.ReactNode }) => {
-    const { isPublicRoute } = useRouterWithOptimisticPathname()
-    return (
-        <div className="flex flex-col w-full h-full">
-            {!isPublicRoute && <Navbar />}
-            {children}
-        </div>
-    )
-}
+export const ProtectedPageLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { isPublicRoute } = useRouterWithOptimisticPathname();
+  return (
+    <div className="flex flex-col w-full h-full items-center justify-center">
+      {!isPublicRoute && <Navbar />}
+      <div className="flex flex-col w-full h-full max-w-[1280px] grid place-items-center">
+        {children}
+      </div>
+    </div>
+  );
+};
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,18 @@
-'use client';
+"use client";
+import { useAppSelector } from "@/lib/hooks";
 import { WelcomeMessage } from "./WelcomeMessage";
+import AdminPage from "../components/AdminPage";
 
-export default function MemberPage() {
-  return <div className="flex flex-col items-center">
-    <WelcomeMessage/>
-  </div>;
+export default function HomePage() {
+  const role = useAppSelector((state) => state.auth.role);
+
+  if (role === "admin") {
+    return <AdminPage />;
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <WelcomeMessage />
+    </div>
+  );
 }
