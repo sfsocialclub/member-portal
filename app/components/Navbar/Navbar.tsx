2fd645424de5d7f72deffb04ef8a0b8723720766
Matthew Galang <magalang1@gmail.com>
2025-08-25 17:05:45 -0700

    Refactor and add menu links Closes #67

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,6 +6,45 @@ import { Avatar } from "./Avatar";
 import AnnouncementIcon from '@mui/icons-material/Announcement';
 import Tooltip from "@mui/material/Tooltip";
 
+const menuItems = [
+  {
+    label: 'Home',
+    href: '/home',
+  },
+  {
+    label: 'Member ID',
+    href: '/qr',
+  },
+  {
+    label: 'Calendar',
+    href: '/calendar',
+  },
+  {
+    label: 'Member Drive',
+    href: 'https://drive.google.com/drive/folders/1imR6adO5qQDOcoekqQ8226GCA-uDDBcy?usp=drive_link',
+    target: '_blank'
+  },
+  {
+    label: 'SF Social Club Shop',
+    href:'https://www.sfsocialclub.org/?fbclid=PAZXh0bgNhZW0CMTEAAafO52FJKLKJkbPBnmuBCGdTA0NjFBLQQ6d8QshyW-gNum3z7e5RzoR57N5IGw_aem_maYih4Gih0rqLLpdGNVDcA',
+    target: '_blank'
+  },
+  {
+    label: 'Instagram',
+    href: 'https://www.instagram.com/sf.socialclub',
+    target: '_blank'
+  },
+  // {
+  //   label: 'Point Shop',
+  //   href:'/point-shop',
+  // },
+  {
+    label: 'Admin',
+    href: '/admin',
+    adminOnly: true
+  }
+]
+
 export const Navbar = () => {
   const session = useAppSession();
   const pathname = usePathname();
@@ -37,56 +76,37 @@ export const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-white rounded-lg z-1 mt-3 w-52 p-2 shadow">
-            <li onClick={handleMenuItemClick}><Link  className={`${pathname === '/home' ? 'font-bold link-primary' : ''}`} href="/home">
-              Home
-            </Link></li>
-            <li onClick={handleMenuItemClick}><Link className={`${pathname === '/qr' ? 'font-bold link-primary' : ''}`} href="/qr">
-              Member ID
-            </Link></li>
-            <li onClick={handleMenuItemClick}><Link className={`${pathname === '/calendar' ? 'font-bold link-primary' : ''}`} href="/calendar">
-              Calendar
-            </Link></li>
-            {/* <li onClick={handleMenuItemClick}><Link className={`${pathname === '/point-shop' ? 'font-bold link-primary' : ''}`} href="/point-shop">
-              Point shop
-            </Link></li> */}
-            {
-              session.user.isAdmin && (
-                <li onClick={handleMenuItemClick}><Link
-                  className={`${pathname === '/admin' ? 'font-bold link-primary' : ''}`}
-                  href="/admin"
-                >
-                  Admin
-                </Link></li>
-              )
-            }
+            className="menu menu-sm dropdown-content bg-white rounded-lg z-1 mt-3 w-max p-2 shadow">
+              {
+                menuItems.filter((item) => !item.adminOnly || session.user.isAdmin).map((item) => (
+                  <li key={item.label} onClick={handleMenuItemClick}>
+                    <Link
+                      className={`${item.href === pathname ? 'font-bold link-primary' : ''}`}
+                      href={item.href}
+                      target={item.target}
+                    >
+                      {item.label}
+                    </Link>
+                  </li>
+                ))
+              }
+            
           </ul>
         </div>
         <div className="shrink-0 flex justify-center lg:w-80">
           <Link className="text-xl uppercase font-bold font-[Red_Hat_Display]" href="/home">SF Social Club</Link>
         </div>
         <div className="gap-x-4 lg:flex hidden">
-          <Link className={`${pathname === '/home' ? 'font-bold link-primary' : ''}`} href="/home">
-            Home
-          </Link>
-          <Link className={`${pathname === '/qr' ? 'font-bold link-primary' : ''}`} href="/qr">
-            Member ID
-          </Link>
-          <Link className={`${pathname === '/calendar' ? 'font-bold link-primary' : ''}`} href="/calendar">
-            Calendar
-          </Link>
-          {/* <Link className={`${pathname === '/point-shop' ? 'font-bold link-primary' : ''}`} href="/point-shop">
-            Point shop
-          </Link> */}
           {
-            session.user.isAdmin && (
+            menuItems.filter((item) => !item.adminOnly || session.user.isAdmin).map((item) => (
               <Link
-                className={`${pathname === '/admin' ? 'font-bold link-primary' : ''}`}
-                href="/admin"
+                key={item.label}
+                className={`${item.href === pathname ? 'font-bold link-primary' : ''}`}
+                href={item.href}
               >
-                Admin
+                {item.label}
               </Link>
-            )
+            ))
           }
         </div>
       </div>
