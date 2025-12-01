52e7c2d065b5b01325410f6dfdadaf40f04600bc
Matthew Galang <magalang1@gmail.com>
2025-12-01 12:45:29 -0800

    Add pull to refresh. closes #81

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,20 +2,27 @@
 import { store } from "@/lib/store";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
+import { PullToRefresh } from "./components/PullToRefresh";
+import { OnboardingWrapper } from "./OnboardingWrapper";
 import { ProtectedPageLayout } from "./ProtectedPageLayout";
 import ProtectedRouteProvider from "./ProtectedRouteProvider";
-import { OnboardingWrapper } from "./OnboardingWrapper";
 
 export const Providers = ({ children, session }: { children: React.ReactNode, session: any }) => {
+
+    const handleRefresh = async () => {
+        window.location.reload();
+    };
     return (
         <SessionProvider session={session} basePath="/api/auth">
             <Provider store={store}>
                 <OnboardingWrapper>
-                    <ProtectedRouteProvider>
-                        <ProtectedPageLayout>
-                            {children}
-                        </ProtectedPageLayout>
-                    </ProtectedRouteProvider>
+                    <PullToRefresh onRefresh={handleRefresh}>
+                        <ProtectedRouteProvider>
+                            <ProtectedPageLayout>
+                                {children}
+                            </ProtectedPageLayout>
+                        </ProtectedRouteProvider>
+                    </PullToRefresh>
                 </OnboardingWrapper>
             </Provider>
         </SessionProvider>
